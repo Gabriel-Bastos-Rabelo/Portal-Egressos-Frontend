ba8184e1f49307cb7be927ba3e0b1b0e2e567f9d
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-02-17 15:25:10 -0300

    feat: adiciona estrutura páginas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="flex min-h-screen justify-center">
+      <div className="container">
+        <h2>Home Page</h2>
+        <p>Aqui terá nosso conteúdo top!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
