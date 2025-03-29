41f4bd7b90fc0f957b8c0698e59213a3fbec4c6c
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-03-29 16:18:30 -0300

    feat: atualiza card depoimento

--- src/components/Cards/DepoimentoCard.tsx
+++ src/components/Cards/DepoimentoCard.tsx
@@ -7,8 +7,8 @@ const DepoimentoCard = ({ depoimento }: { depoimento: Depoimento }) => {
       <img className= "rounded-full w-32 h-32" src={egressoImg} alt="Foto do egresso" />
       <div className="flex flex-col">
         <h3 className="text-xl">{depoimento.nomeEgresso}</h3>
-        <p className="text-gray-600">{depoimento.nomeCurso}</p>
-        <p className="text-gray-600 text-justify">{depoimento.texto}</p>
+        <p className="text-gray-600">{depoimento.curso}</p>
+        <p className="text-gray-600 text-justify">{depoimento.descricao}</p>
       </div>
     </div>
   );
