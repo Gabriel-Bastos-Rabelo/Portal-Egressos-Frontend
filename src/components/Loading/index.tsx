24a0d63c9b8ba4b1dbee4dd268dd6de03200287c
Sabryna Rodrigues <130665935+SabrynaS@users.noreply.github.com>
2025-03-30 12:39:45 -0300

    feat: add loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-96">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+      <p className="ml-4 text-xl font-semibold text-gray-700">Carregando...</p>
+    </div>
+  );
+}
+  
+export default Loading;
--- src/pages/Estatisticas/Estatisticas.tsx
+++ src/pages/Estatisticas/Estatisticas.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Pie, Bar } from 'react-chartjs-2';
+import Loading from '../../components/Loading/index.tsx';
 import { EstatisticasService } from '../../services/estatisticasService';
 
 import {
@@ -24,6 +25,7 @@ ChartJS.register(
 );
 
 const Estatisticas: React.FC = () => {
+  const [loading, setLoading] = useState(true);
   const [totalEgressos, setTotalEgressos] = useState(0);
   const [porCurso, setPorCurso] = useState<{ [nivel: string]: number }>({});
   const [cargos, setCargos] = useState<{ [descricao: string]: number }>({});
@@ -34,6 +36,7 @@ const Estatisticas: React.FC = () => {
     async function carregarDados() {
       const egressos = await EstatisticasService.getEgressosAprovados();
       setTotalEgressos(egressos.length);
+      setLoading(false);
 
       const cursoMap: { [nivel: string]: number } = {};
       const cargoMap: { [descricao: string]: number } = {};
@@ -100,21 +103,93 @@ const Estatisticas: React.FC = () => {
     <div className="container mx-auto p-4 flex gap-20 flex-col itens-center justify-center">
       <h1 className="text-2xl font-bold text-center">Estatísticas Importantes dos Egressos</h1>
 
-      {/* Total de Egressos */}
-      <div className="flex flex-wrap gap-12 justify-center">
-        <div className='flex flex-col gap-[50px]'>
-          <div className="w-[300px] h-[150px] bg-white p-4 rounded-lg shadow-md text-center ">
-            <h3 className="w-full text-xl font-semibold text-center mb-4">TOTAL DE EGRESSOS </h3>
-            <p className="text-4xl font-bold text-orange-600">{totalEgressos}</p>
-          </div>
-        </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {/* Total de Egressos */}
+          <div className="flex flex-wrap gap-12 justify-center">
+            <div className='flex flex-col gap-[50px]'>
+              <div className="w-[300px] h-[150px] bg-white p-4 rounded-lg shadow-md text-center ">
+                <h3 className="w-full text-xl font-semibold text-center mb-4">TOTAL DE EGRESSOS </h3>
+                <p className="text-4xl font-bold text-orange-600">{totalEgressos}</p>
+              </div>
+            </div>
 
-        {/* Total de Egressos por Curso */}
-        <div className="w-[700px] h-[350px] bg-white p-0 rounded-lg shadow-md text-center flex items-center justify-center">
-          <div className="flex flex-col items-center justify-center">
-            <h3 className="text-xl font-semibold text-center mb-0">TOTAL DE EGRESSOS POR CURSO</h3>
-            <div className="w-[350px] h-[350px] flex items-center justify-center">
-              <Pie data={totalEgressosPorCursoData} options={{
+            {/* Total de Egressos por Curso */}
+            <div className="w-[700px] h-[350px] bg-white p-0 rounded-lg shadow-md text-center flex items-center justify-center">
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="text-xl font-semibold text-center mb-0">TOTAL DE EGRESSOS POR CURSO</h3>
+                <div className="w-[350px] h-[350px] flex items-center justify-center">
+                  <Pie data={totalEgressosPorCursoData} options={{
+                    plugins: {
+                      legend: {
+                        position: 'right',
+                        labels: {
+                          boxWidth: 15,
+                          padding: 10,
+                        },
+                      },
+                    },
+                  }} />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-row justify-center gap-20 mb-8'>
+            {/* Cargos por Egressos */}
+            <div className="w-full max-w-[480px] bg-white p-4 rounded-lg shadow-md">
+              <h3 className="text-xl font-semibold text-center mb-4">CARGOS POR EGRESSOS</h3>
+              <div className="w-full h-auto max-h-[490px] overflow-auto">
+                <Bar
+                  data={cargosPorEgressosData}
+                  options={{
+                    responsive: true,
+                    plugins: {
+                      tooltip: {
+                        mode: 'index',
+                        intersect: false,
+                      },
+                      legend: {
+                        display: false
+                      }
+                    },
+                    indexAxis: 'y',
+                    maintainAspectRatio: false,
+                    scales: {
+                      x: { stacked: true,
+                        beginAtZero: true,
+                        suggestedMin: 0,
+                        suggestedMax: Math.max(...Object.values(cargos)) <= 4 ? 4 : Math.ceil(Math.max(...Object.values(cargos)) + 1),
+                        ticks: {
+                          stepSize:2,
+                          maxTicksLimit: 20,
+                          padding: 0
+
+                        },
+                        grid: {
+                          drawTicks: false
+                        }
+                      },
+                
+                      y: {
+                        stacked: true,
+                        beginAtZero: true,
+                        ticks: {
+                          autoSkip: false
+                        }
+                      },
+                    },
+                  }}
+                  height={Math.max(Object.keys(cargos).length * 30, 500)}
+                />
+              </div>
+            </div>
+  
+            <div className="w-[480px] h-[470px] bg-white p-4 rounded-lg shadow-md text-center">
+              <h3 className="text-xl font-semibold">TAXA DE EMPREGABILIDADE</h3>
+              <Pie data={taxaDeEmpregabilidadeData} options={{
                 plugins: {
                   legend: {
                     position: 'right',
@@ -127,75 +202,9 @@ const Estatisticas: React.FC = () => {
               }} />
             </div>
           </div>
-        </div>
-      </div>
-
-      <div className='flex flex-row justify-center gap-20 mb-8'>
-        {/* Cargos por Egressos */}
-        <div className="w-full max-w-[480px] bg-white p-4 rounded-lg shadow-md">
-          <h3 className="text-xl font-semibold text-center mb-4">CARGOS POR EGRESSOS</h3>
-          <div className="w-full h-auto max-h-[490px] overflow-auto">
-            <Bar
-              data={cargosPorEgressosData}
-              options={{
-                responsive: true,
-                plugins: {
-                  tooltip: {
-                    mode: 'index',
-                    intersect: false,
-                  },
-                  legend: {
-                    display: false
-                  }
-                },
-                indexAxis: 'y',
-                maintainAspectRatio: false,
-                scales: {
-                  x: { stacked: true,
-                    beginAtZero: true,
-                    suggestedMin: 0,
-                    suggestedMax: Math.max(...Object.values(cargos)) <= 4 ? 4 : Math.ceil(Math.max(...Object.values(cargos)) + 1),
-                    ticks: {
-                      stepSize:2,
-                      maxTicksLimit: 20,
-                      padding: 0
-
-                    },
-                    grid: {
-                      drawTicks: false
-                    }
-                  },
-                
-                  y: {
-                    stacked: true,
-                    beginAtZero: true,
-                    ticks: {
-                      autoSkip: false
-                    }
-                  },
-                },
-              }}
-              height={Math.max(Object.keys(cargos).length * 30, 500)}
-            />
-          </div>
-        </div>
-  
-        <div className="w-[480px] h-[470px] bg-white p-4 rounded-lg shadow-md text-center">
-          <h3 className="text-xl font-semibold">TAXA DE EMPREGABILIDADE</h3>
-          <Pie data={taxaDeEmpregabilidadeData} options={{
-            plugins: {
-              legend: {
-                position: 'right',
-                labels: {
-                  boxWidth: 15,
-                  padding: 10,
-                },
-              },
-            },
-          }} />
-        </div>
-      </div>
-    </div>    
+        </>
+      )}
+    </div>        
   );
 };
 
