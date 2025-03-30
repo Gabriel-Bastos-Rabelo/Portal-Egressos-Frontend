e4ff6c95559c9d36b211b650a3184bc413d08a66
anddersonls <andersonlsilva03@gmail.com>
2025-03-30 09:48:48 -0300

    feat: ajustes do merge para englobar paginação de dados filtrados

--- src/pages/Oportunidades/index.tsx
+++ src/pages/Oportunidades/index.tsx
@@ -35,8 +35,6 @@ function Oportunidades() {
       });
   }, []);
 
-  const paginatedOportunidades = paginate(oportunidades, page, itemsPerPage);
-
   const handleFiltrar = () => {
     setFilters({
       titulo: tituloInput,
@@ -61,23 +59,21 @@ function Oportunidades() {
   
     return tituloMatch && tipoMatch && dataMatch;
   });
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(oportunidadesFiltradas.length / itemsPerPage));
+  }, [oportunidadesFiltradas]);
+  
+  const paginatedOportunidades = paginate(oportunidadesFiltradas, page, itemsPerPage);
   
   return (
     <div className="flex min-h-screen w-screen justify-center my-12">
       <div className="flex flex-col items-center gap-8">
-        <h1 className="text-3xl font-bold text-center mt-12">Oportunidades</h1>
+        <h1 className="text-3xl font-bold text-center">Oportunidades</h1>
         
         {loading ? (
           <Loading />
         ) : (
-          <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-              {paginatedOportunidades.map((oportunidade) => (
-                <OportunidadeCard key={oportunidade.id} oportunidade={oportunidade} />
-              ))}
-            </div>
-            
-          </>
           <>
             <div className="flex flex-wrap gap-4 items-end justify-center mb-10">
               <div className="flex flex-col">
@@ -141,7 +137,7 @@ function Oportunidades() {
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-              {oportunidadesFiltradas.map((oportunidade) => (
+              {paginatedOportunidades.map((oportunidade) => (
                 <OportunidadeCard 
                   key={oportunidade.id} 
                   oportunidade={oportunidade} 
